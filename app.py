740bd22ae69d6bc1859677d82c637a5c085749dc
Chas McCammon (1000007418) <MCCACJ3@student.op.ac.nz>
2019-03-20 08:57:16 +1300

    Adding API's

--- app.py
+++ app.py
@@ -34,6 +34,22 @@ def inspirations():
     myName = "Chas"
     return render_template('inspirations.html', name=myName)
 
+@app.route('/countries/', methods=['GET'])
+@app.route('/countries/<country_id>', methods=['GET'])
+def getCountries(country_id=None):
+    country = None
+    if country_id is None:
+        country = Country.objects
+    else:
+        country = Country.objects.get(id=country_id)
+    return country.to_json()
+
+@app.route('/deleteCountry/<country_id>', methods=['DELETE'])
+def deleteCountries():
+    Country.objects.delete(id=country_id)
+    return render_template('index.html')
+
+
 
 if __name__ =="__main__":
     app.run(debug=True,port=8080)
